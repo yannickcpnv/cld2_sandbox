116f42aeb075cad4b419e66a0526a87e4eadedca
Yannick Baudraz <yannick.baudraz@cpnv.ch>
2022-03-04 12:10:49 +0100

    Add image in picture show page

--- resources/views/galleries/pictures/show.blade.php
+++ resources/views/galleries/pictures/show.blade.php
@@ -2,6 +2,10 @@
     <x-slot name="titlePage">Picture N°{{ $picture->id }}</x-slot>
     <a href="{{ route('galleries.pictures.index', $gallery) }}">All pictures in {{ $gallery->name }}</a>
     <p>Title : {{ $picture->title }} </p>
+    <img alt="{{ $picture->title }}"
+         style="max-width: 500px; height: auto"
+         src="{{ Storage::url($picture->path) }}"
+    >
     <form method="post" action="{{ route('galleries.pictures.destroy', [$gallery, $picture])}}">
         @method('delete')
         @csrf
