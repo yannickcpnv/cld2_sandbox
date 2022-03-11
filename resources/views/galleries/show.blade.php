4ce9bab604d6c66b1afeb534161404b9356a41f5
Yannick Baudraz <yannick.baudraz@cpnv.ch>
2022-03-11 08:54:30 +0100

    Remove gallery.show view

--- resources/views/galleries/show.blade.php
+++
@@ -1,6 +0,0 @@
-<x-layout.app>
-    <x-slot name="titlePage">Gallery N°{{ $gallery->id }}</x-slot>
-    <a href="{{ route('galleries.index') }}">All galleries</a>
-    <p>Name : {{ $gallery->name }} </p>
-    <a href="{{ route('galleries.pictures.index', $gallery) }}">See pictures ({{ $gallery->pictures->count() }})</a>
-</x-layout.app>
